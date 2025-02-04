3082398607c5c2206a5f1449d56e5dacd6eca825
gsilva.inatel <guilherme.cesar@inatel.br>
2025-02-04 18:47:37 -0300

    feat: add filter to open units.

--- desafio-smart/src/app/components/forms/forms.component.ts
+++ desafio-smart/src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from 'src/app/interfaces/location.interface';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 
 @Component({
@@ -8,7 +9,8 @@ import { GetUnitsService } from 'src/app/services/get-units.service';
   styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(
@@ -17,16 +19,25 @@ export class FormsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.unitService
-      .getAllUnits()
-      .subscribe((data) => console.log('data', data));
+    this.unitService.getAllUnits().subscribe((data) => {
+      this.results = data.locations;
+      this.filteredResults = data.locations;
+    });
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false,
+      showClosed: true,
     });
   }
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    if (!this.formGroup.value.showClosed) {
+      this.filteredResults = this.results.filter(
+        (location) => location.opened === true
+      );
+    } else {
+      this.filteredResults = this.results;
+    }
+  }
 
   onClean(): void {
     this.formGroup.reset();
